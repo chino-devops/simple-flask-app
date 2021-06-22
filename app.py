1c7295fd8de5ffbcabce80e20ce2e4216e04c348
Luke Benson <55387287+LukeBenson@users.noreply.github.com>
2021-06-22 14:57:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from os import getenv
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World"
+    return getenv('mytext')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
